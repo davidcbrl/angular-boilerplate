109c8a9a0544ca2d303d99a067a519c552ecacbc
David Cabral <david.pereira@tecnofit.com.br>
2021-07-05 10:49:28 -0300

    Importing code

---
+++ projects/fidelity/src/app/app-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthGuard } from '@fidelity/guards/auth/auth.guard';
+import { SplashComponent } from '@fidelity/components/splash/splash.component';
+import { AuthComponent } from '@fidelity/components/auth/auth.component';
+import { NotFoundComponent } from '@fidelity/components/not-found/not-found.component';
+import { DashboardComponent } from '@fidelity/components/dashboard/dashboard.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+
+const ROUTES: Routes = [
+  {
+    path: '',
+    component: SplashComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'product',
+    component: ProductComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'product/:id',
+    component: ProductFormComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'product/new',
+    component: ProductFormComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(ROUTES)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
